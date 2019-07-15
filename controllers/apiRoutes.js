37ad9bb4c5e686becfb270347f9d677c0a32a584
monica <mogimartinez@gmail.com>
2019-07-14 23:40:13 -0500

    added routes + form

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,6 +1,25 @@
 const movies = require("../data/movieData");
 
 module.exports = function (app) {
-    
-    
+
+    app.get("/", function (req, res) {
+        res.render("home");
+    });
+
+    app.get("/movies", function (req, res) {
+        //movieData = what you want to call | movies = data being passed in
+        res.render("movieList", {
+            movieData: movies
+        });
+    });
+
+    app.post("/movies", function (req, res) {
+        const movieName = req.body.movieName;
+        const image = req.body.image;
+        let newMovie = {name: movieName, image: image};
+        
+        movies.push(newMovie);
+
+        res.redirect("/movies")
+    });
 }
